14dcb5dd4f1ff402edc6c7706e1d5de9ac791ec6
BogdanChumak96 <bogdanchumak96@gmail.com>
2025-04-27 11:32:09 +0300

    fixed bug with store

--- src/store/game-store.js
+++ src/store/game-store.js
@@ -15,7 +15,7 @@ export const useStore = create((set, get) => ({
 
   setCurrentLetter: (letter) => set(() => ({ currentLetter: letter })),
   setGuessesLetters: (letter) =>
-    set(() => ({ guessesLetters: [...state.guessesLetters, letter] })),
+    set((state) => ({ guessesLetters: [...state.guessesLetters, letter] })),
 
   minusHealth: () => set((state) => ({ health: state.health - 1 })),
   resetGame: () =>
@@ -23,30 +23,23 @@ export const useStore = create((set, get) => ({
       randomWord: words[Math.floor(Math.random() * words.length)],
       health: 3,
     })),
-  setStartGame: () =>
-    set(() => {
+    setStartGame: () => {
       const randomWord = words[Math.floor(Math.random() * words.length)];
       const difficult = get().difficult;
       let health = 4;
-
-      if (difficult === "Easy") {
-        health = 4;
-      }
-      if (difficult === "Medium") {
-        health = 3;
-      }
-      if (difficult === "Hard") {
-        health = 2;
-      }
+      if (difficult === "Easy")  health = 4;
+      if (difficult === "Medium") health = 3;
+      if (difficult === "Hard") health = 2;
+    
       set({
         randomWord,
         hideWord: randomWord.replace(/[a-zA-Zа]/g, "‎"),
         health,
         guessesLetters: [],
-        startGame:true,
-        currentLetter:"",
+        startGame: true,
+        currentLetter: "",
       });
-    }),
+    },
 
   setDifficult: (difficult) => set(() => ({ difficult })),
 }));
